9988dc3f49b0cb88cf57fe33661066ad5068bf90
admin <tienhung@cfco.com.vn>
2020-03-23 10:32:17 +0700

    SERVER_NOTI

--- app/src/main/java/vn/cservn/SERVER_NOTI.java
+++ app/src/main/java/vn/cservn/SERVER_NOTI.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 public class SERVER_NOTI {
     public  void  run(final Result result, final Callback21 callback21){
@@ -24,15 +25,25 @@ public class SERVER_NOTI {
                     byte[] bytes = IOUtils.toByteArray(in);
                     String str = new String(bytes, "UTF-8");
 
+
                     result.success =true;
-                    result.data = str;
-                    callback21.result(result);
 
                 } catch (IOException e) {
                     e.printStackTrace();
+                    result.error = e.getMessage();
+                    result.success =false;
                 }
+                callback21.result(result);
 
             }
         }.run();
+    }
+
+    public  void  run(){
+
+    }
+
+    class Data{
+        List<Noti21> notis;
     }
 }
